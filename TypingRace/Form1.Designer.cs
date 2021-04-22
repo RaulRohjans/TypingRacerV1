50a13916d24d7fa1e9c03204c4051b284ae0f54a
RaulRohjans <77687494+RaulRohjans@users.noreply.github.com>
2021-04-22 20:28:03 +0100

    project

---
+++ TypingRace/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace TypingRace
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.txt_word = new System.Windows.Forms.TextBox();
+            this.txt_text = new System.Windows.Forms.TextBox();
+            this.lbl_mins = new System.Windows.Forms.Label();
+            this.btn_exit = new System.Windows.Forms.Button();
+            this.txt_tempo = new System.Windows.Forms.TextBox();
+            this.btn_start = new System.Windows.Forms.Button();
+            this.lbl_tempo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "iconfinder_Keyboard_728976.png");
+            // 
+            // txt_word
+            // 
+            this.txt_word.Location = new System.Drawing.Point(164, 297);
+            this.txt_word.Name = "txt_word";
+            this.txt_word.Size = new System.Drawing.Size(304, 22);
+            this.txt_word.TabIndex = 0;
+            this.txt_word.TextChanged += new System.EventHandler(this.txt_word_TextChanged);
+            this.txt_word.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // txt_text
+            // 
+            this.txt_text.Location = new System.Drawing.Point(12, 12);
+            this.txt_text.Multiline = true;
+            this.txt_text.Name = "txt_text";
+            this.txt_text.Size = new System.Drawing.Size(655, 226);
+            this.txt_text.TabIndex = 1;
+            // 
+            // lbl_mins
+            // 
+            this.lbl_mins.AutoSize = true;
+            this.lbl_mins.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_mins.Location = new System.Drawing.Point(258, 358);
+            this.lbl_mins.Name = "lbl_mins";
+            this.lbl_mins.Size = new System.Drawing.Size(87, 32);
+            this.lbl_mins.TabIndex = 2;
+            this.lbl_mins.Text = "00:00";
+            this.lbl_mins.Visible = false;
+            // 
+            // btn_exit
+            // 
+            this.btn_exit.Location = new System.Drawing.Point(548, 556);
+            this.btn_exit.Name = "btn_exit";
+            this.btn_exit.Size = new System.Drawing.Size(119, 23);
+            this.btn_exit.TabIndex = 3;
+            this.btn_exit.Text = "Sair";
+            this.btn_exit.UseVisualStyleBackColor = true;
+            this.btn_exit.Click += new System.EventHandler(this.btn_exit_Click);
+            // 
+            // txt_tempo
+            // 
+            this.txt_tempo.Location = new System.Drawing.Point(264, 368);
+            this.txt_tempo.Name = "txt_tempo";
+            this.txt_tempo.Size = new System.Drawing.Size(81, 22);
+            this.txt_tempo.TabIndex = 4;
+            this.txt_tempo.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_tempo_KeyPress);
+            // 
+            // btn_start
+            // 
+            this.btn_start.Location = new System.Drawing.Point(245, 410);
+            this.btn_start.Name = "btn_start";
+            this.btn_start.Size = new System.Drawing.Size(119, 23);
+            this.btn_start.TabIndex = 5;
+            this.btn_start.Text = "Start";
+            this.btn_start.UseVisualStyleBackColor = true;
+            this.btn_start.Click += new System.EventHandler(this.btn_start_Click);
+            // 
+            // lbl_tempo
+            // 
+            this.lbl_tempo.AutoSize = true;
+            this.lbl_tempo.Location = new System.Drawing.Point(261, 341);
+            this.lbl_tempo.Name = "lbl_tempo";
+            this.lbl_tempo.Size = new System.Drawing.Size(56, 17);
+            this.lbl_tempo.TabIndex = 6;
+            this.lbl_tempo.Text = "Tempo:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(679, 591);
+            this.Controls.Add(this.lbl_tempo);
+            this.Controls.Add(this.btn_start);
+            this.Controls.Add(this.txt_tempo);
+            this.Controls.Add(this.btn_exit);
+            this.Controls.Add(this.lbl_mins);
+            this.Controls.Add(this.txt_text);
+            this.Controls.Add(this.txt_word);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Typing Race";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.TextBox txt_word;
+        private System.Windows.Forms.TextBox txt_text;
+        private System.Windows.Forms.Label lbl_mins;
+        private System.Windows.Forms.Button btn_exit;
+        private System.Windows.Forms.TextBox txt_tempo;
+        private System.Windows.Forms.Button btn_start;
+        private System.Windows.Forms.Label lbl_tempo;
+    }
+}
+
---
+++ TypingRace/Form1.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+
+namespace TypingRace
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        bool correto = true;
+        char[] texto;
+
+        int m, s;
+        int totalMins;
+        int cntPalavras = 0;
+
+        System.Timers.Timer t;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            t = new System.Timers.Timer();
+            t.Interval = 1000;
+            t.Elapsed += OnTimeEvent;
+        }
+
+        private void OnTimeEvent(object sender, ElapsedEventArgs e)
+        {
+            Invoke(new Action(() =>
+            {
+                s--;
+                if (m == 0 && s == 0)
+                {
+                    t.Stop();
+                    lbl_mins.Text = "00:00";
+                    lbl_mins.Visible = false;
+                    txt_tempo.Visible = true;
+                    btn_start.Enabled = true;
+                    MessageBox.Show("A sua velocidade media foi: " + Convert.ToString((int) cntPalavras / totalMins), "Parabens!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    if (s == 0)
+                    {
+                        s = 60;
+                        m--;
+                    }
+
+                    if (m > 9 && s > 9)
+                        lbl_mins.Text = m.ToString() + ":" + s.ToString();
+                    else
+                    {
+                        if (m > 9 && s <= 9)
+                            lbl_mins.Text = m.ToString() + ":" + "0" + s.ToString();
+                        else
+                        {
+                            if (m <= 9 && s > 9)
+                                lbl_mins.Text = "0" + m.ToString() + ":" + s.ToString();
+                            else
+                                lbl_mins.Text = "0" + m.ToString() + ":" + "0" + s.ToString();
+                        }
+                    }
+                }
+
+            }));
+        }
+
+        private void RemoverArray()
+        {
+            for(int i2 = 0; i2 < texto.Length - 1; i2++)
+            {
+                texto[i2] = texto[i2 + 1];
+            }
+            texto[texto.Length - 1] = ' ';
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            try
+            {
+                if (e.KeyChar == ' ' && correto == true)
+                {
+                    if (texto[txt_word.TextLength] == ' ' || texto[txt_word.TextLength] == '\n' || texto[txt_word.TextLength] == '\r')
+                    {
+                        while (texto[0] == ' ')
+                            RemoverArray();
+                        while (texto[0] == '\r')
+                            RemoverArray();
+                        while (texto[0] == '\n')
+                            RemoverArray();
+
+                        //Apagar palavra do array mais o espaco                        
+                        for (int i = 0; i < txt_word.TextLength; i++)
+                        {
+                            RemoverArray();
+                        }                               
+                        
+                        while (texto[0] == ' ')
+                            RemoverArray();                        
+                        while (texto[0] == '\r')
+                            RemoverArray();
+                        while (texto[0] == '\n')
+                            RemoverArray();
+
+                        //Resetar textbox
+                        txt_word.Text = "";
+                        e.Handled = true;
+                        cntPalavras++;
+
+                        //Colocar coisas na textbox outravez
+                        txt_text.Text = "";
+                        for(int i = 0; i < texto.Length; i++)
+                        {
+                            txt_text.Text += texto[i];
+                        }
+                    }
+                    else
+                    {
+                        correto = true;
+                        txt_word.BackColor = Color.White;
+                    }
+                }
+            }
+            catch(Exception ee)
+            {
+                MessageBox.Show("Erro:  " + ee);
+            }
+        }
+
+        private void btn_exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private Int32 CountFileChars(string path)
+        {
+            Int32 Count = -1;
+
+            using (System.IO.StreamReader sr = new System.IO.StreamReader(path))
+            {
+                string content = sr.ReadToEnd();
+                Count = content.Length;
+            }
+                return Count;
+        }
+
+        private void btn_start_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txt_tempo.Text != "" && (Convert.ToInt32(txt_tempo.Text) > 0 && Convert.ToInt32(txt_tempo.Text) <= 60))
+                {
+                    int i = 0;
+                    string path = "";
+                    totalMins = Convert.ToInt32(txt_tempo.Text);
+                    cntPalavras = 0;
+
+                    //Carregar coisas para o Array de Chars
+                    OpenFileDialog openFileDialog = new OpenFileDialog();
+                    openFileDialog.InitialDirectory = "c:\\";
+                    openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                    openFileDialog.FilterIndex = 2;
+                    openFileDialog.RestoreDirectory = true;
+
+                    if (openFileDialog.ShowDialog() == DialogResult.OK)
+                    {                        
+                        path = openFileDialog.FileName;
+                        Int32 qtdChars = CountFileChars(path);
+                        if (qtdChars != -1)
+                        {
+                            texto = new char[qtdChars];
+
+                            StreamReader sr = new StreamReader(path);
+                            while (!sr.EndOfStream)
+                            {
+                                texto[i] += (char)sr.Read();
+                                txt_text.Text += texto[i];
+                                i++;
+                            }
+                            sr.Close();                         
+                                
+
+                            //Parte dos numeros e horas
+                            txt_tempo.Visible = false;
+                            lbl_mins.Visible = true;
+                            m = Convert.ToInt32(txt_tempo.Text) - 1;
+                            s = 60;
+                            t.Start();
+
+                            btn_start.Enabled = false;
+                        }
+                        else
+                            MessageBox.Show("Ocorreu um erro na contagem de caracteres do ficheiro!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    else
+                        MessageBox.Show("Ocorreu um erro na leitura do ficheiro, por favor tente novamente!", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+
+                }
+                else
+                    MessageBox.Show("A caixa de texto deve conter um numero em minutos entre 0 e 60!", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch(Exception ee)
+            {
+                MessageBox.Show("Erro:  " + ee);
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            t.Stop();
+        }
+
+        private void txt_word_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                if(txt_word.Text != "")
+                {
+                    char[] temp = txt_word.Text.ToCharArray();
+                    if (texto[txt_word.TextLength - 1] != temp[txt_word.TextLength - 1])
+                    {
+                        correto = false;
+                        txt_word.BackColor = Color.Red;
+                    }
+                    else
+                    {
+                        correto = true;
+                        txt_word.BackColor = Color.White;
+                    }
+                }
+                else
+                {
+                    correto = true;
+                    txt_word.BackColor = Color.White;
+                }                
+            }
+            catch(Exception ee)
+            {
+                MessageBox.Show("Erro: " + ee);
+            }
+        }
+
+        private void txt_tempo_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar < '0' || e.KeyChar > '9') && e.KeyChar != 8)
+                e.Handled = true;
+        }        
+    }
+}
